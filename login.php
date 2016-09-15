0399a6c2b330ddc9b98e19575f817ab2217170b0
alekbaht <alekbaht@greeny.cs.tlu.ee>
2016-09-15 12:43:59 +0300

    test

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+
+	//var_dump($_GET) ;
+	//echo "<br>";
+	//var_dump($_POST) ;
+	
+	$signupEmailError = "";
+	
+	//$_POST["signupEmail"]
+	
+	//kas on yldse olemas
+	if (isset ($_POST["signupEmail"])) {
+		
+		//oli olemas, ehk keegi vajutas nuppu
+
+		if (empty($_POST["signupEmail"])) {
+			
+			//oli toesti tyhi 
+			$signupEmailError = "See väli on kohustuslik";
+		}
+	}
+	
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,17 +31,34 @@
 
 		<h1>Logi sisse</h1>
 		
-		<form>
+		<form method="POST">
 		
-			<input type="email">
+			<input placeholder="email" name="loginEmail" type="email">
 			
 			<br><br>
 
-			<input type="password">
+			<input placeholder="Parool" name="loginPassword" type="password">
 			
 			<br><br>
 			
 			<input type ="submit">
+			
+			
+		</form>
+		
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST">
+			
+			<input placeholder="email" name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+
+			<input placeholder="Parool" name="signupPassword" type="password">
+			
+			<br><br>
+			
+			<input type="submit" value="Loo kasutaja">
 			
 			
 		</form>
